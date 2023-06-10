92700c68aed4d5e563f777514d6f73d13259e3fd
khodjiyev2o <samandarkhodjiyev@gmail.com>
2023-06-10 17:48:23 +0500

    fix: response format in room/availability endpoint

--- apps/rooms/api_endpoints/Availability/views.py
+++ apps/rooms/api_endpoints/Availability/views.py
@@ -1,15 +1,15 @@
-from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from apps.rooms.models import Room
 
 
-class RoomAvailabilityRetrieveView(GenericAPIView):
+class RoomAvailabilityRetrieveView(APIView):
     def get(self, request, *args, **kwargs):
         room = Room.objects.get(id=kwargs.get("pk"))
         date = self.request.query_params.get("date")
         availability = room.availability(date=date)
-        return Response({"data": availability})
+        return Response(availability)
 
 
 __all__ = ["RoomAvailabilityRetrieveView"]
