29f5412cdb22d0c976df23c0bed0bd21b6011ea7
khodjiyev2o <samandarkhodjiyev@gmail.com>
2023-06-25 19:51:26 +0500

    fix: minor changes for time slots

--- apps/rooms/utils.py
+++ apps/rooms/utils.py
@@ -9,8 +9,8 @@ def get_availability(self, date=None):
     else:
         date = datetime.strptime(date, "%d-%m-%Y").date()
 
-    start_time = timezone.make_aware(timezone.datetime(date.year, date.month, date.day, 9, 0, 0))
-    end_time = timezone.make_aware(timezone.datetime(date.year, date.month, date.day, 18, 0, 0))
+    start_time = timezone.make_aware(timezone.datetime.combine(date, datetime.min.time()))
+    end_time = timezone.make_aware(timezone.datetime.combine(date, datetime.max.time()))
 
     booked_slots = self.bookings.filter(start__date=date, end__date=date).order_by("start")
     availability = []
@@ -23,7 +23,7 @@ def get_availability(self, date=None):
             }
         )
     else:
-        current_slot_start = start_time.replace(hour=9)
+        current_slot_start = start_time
         for booked_slot in booked_slots:
             if current_slot_start < booked_slot.start:
                 availability.append(
@@ -34,23 +34,16 @@ def get_availability(self, date=None):
                 )
             current_slot_start = booked_slot.end
 
-        if current_slot_start < end_time.replace(hour=18, minute=0, second=0):
+        if current_slot_start < end_time:
             availability.append(
                 {
                     "start": timezone.localtime(current_slot_start).strftime("%d-%m-%Y %H:%M:%S"),
-                    "end": timezone.localtime(end_time.replace(hour=18, minute=0, second=0)).strftime(
-                        "%d-%m-%Y %H:%M:%S"
-                    ),
+                    "end": timezone.localtime(end_time).strftime("%d-%m-%Y %H:%M:%S"),
                 }
             )
 
-    """ if the whole day is booked """
-    if availability == [
-        {
-            "start": timezone.localtime(end_time.replace(hour=18, minute=0, second=0)).strftime("%d-%m-%Y %H:%M:%S"),
-            "end": timezone.localtime(end_time.replace(hour=18, minute=0, second=0)).strftime("%d-%m-%Y %H:%M:%S"),
-        }
-    ]:
-        return {"error": "uzr, siz tanlagan vaqtda xona band"}
+    # Remove the availability slot if it covers the entire day.
+    availability = [slot for slot in availability if slot["start"] != slot["end"]]
 
     return availability
+
